7ea4de8b938c8ab1fc4259eb3b1bde5eb731bddb
xbroindia <155175426+xbroindia@users.noreply.github.com>
2024-09-09 15:31:39 +0530

    updated

---
+++ perfume-product/perfume-product/components/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Card1 from './card1'
+const App = ()=>{
+    return(
+        <div className="main">
+            <Card1/>
+        </div>
+    )}
+export default App
