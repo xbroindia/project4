250934d957dcdf93af6618d41e76f946f30490c9
xbroindia <155175426+xbroindia@users.noreply.github.com>
2024-09-13 18:15:02 +0530

    updated

--- perfume-product/perfume-product/src/app.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from '../components/main'
-let root =ReactDOM.createRoot(document.querySelector('#container'))
-root.render(<App/>)
